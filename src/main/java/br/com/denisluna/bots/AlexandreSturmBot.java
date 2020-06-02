a1c94385054cd4f829e1e0d86b6deafba74f8f9f
Denis Luna <denis.lbs18@gmail.com>
2020-06-02 14:47:05 -0300

    Atualização SturmBot/Maven
    Inclusão de método falouComigo() em classe AlexandreSturmBot.
    Inclusão de parâmetros Java8 em pom.xml.

--- src/main/java/br/com/denisluna/bots/AlexandreSturmBot.java
+++ src/main/java/br/com/denisluna/bots/AlexandreSturmBot.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.denisluna.telegrambots.types.Message;
+import br.com.denisluna.telegrambots.types.MessageEntity;
+import br.com.denisluna.telegrambots.utils.PadraoDeTags;
 
 public class AlexandreSturmBot extends Bot {
 	public static final String TOKEN = "268994751:AAGQUiHc4FpxlGlLPNdTTUciA6TJ0ifB03U";
@@ -14,11 +16,25 @@ public class AlexandreSturmBot extends Bot {
 
 	@Override
 	public void responde(Message mensagem) {
-		List<String> resposta = new ArrayList<String>();
-		resposta.add("Mano");
-		resposta.add("Inter nas semis");
-		resposta.add("Foda...");
-		this.telegram.sendMessage(this.getChatId(), resposta);
+		if (falouComigo(mensagem)) {
+			List<String> resposta = new ArrayList<>();
+			resposta.add("Mano");
+			resposta.add("Inter nas semis");
+			resposta.add("Foda...");
+			this.telegram.sendMessage(this.getChatId(), resposta);
+		}
+	}
+
+	private boolean falouComigo(Message mensagem) {
+		boolean temNome = (mensagem.getText().trim().toUpperCase().contains(this.nomeBot.toUpperCase()));
+		boolean mencao = false;
+		if (mensagem.getEntities() != null) {
+			for (MessageEntity entity : mensagem.getEntities()) {
+				mencao = (entity.getType().equals(PadraoDeTags.TEXT_MENTION))
+						|| (entity.getType().equals(PadraoDeTags.MENTION));
+			}
+		}
+		return (temNome || mencao);
 	}
 
 	@Override
